1f91f04c039903284a8a25d76b50c8caff874b28
karos <329847738@qq.com>
2017-07-10 21:09:32 +0800

    add bg color

--- LKCalendar/LKCalendar/LKCalendarCell.m
+++ LKCalendar/LKCalendar/LKCalendarCell.m
@@ -10,7 +10,9 @@
 
 @interface LKCalendarCell ()
 
+@property (nonatomic, strong) UIView *bgView;
 @property (nonatomic, strong) UILabel *textLabel;
+@property (nonatomic, strong) UIView *eventView;
 
 @end
 
@@ -28,12 +30,16 @@
 }
 
 - (void)setupView {
+    [self.contentView addSubview:self.bgView];
     [self.contentView addSubview:self.textLabel];
+    [self.contentView addSubview:self.eventView];
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
     self.textLabel.frame = self.bounds;
+    self.bgView.frame = CGRectMake((CGRectGetWidth(self.bounds) - 35) / 2, (CGRectGetHeight(self.bounds) - 35) / 2, 35, 35);
+    self.eventView.frame = CGRectMake((CGRectGetWidth(self.bounds) - 5) / 2, CGRectGetHeight(self.bounds) - 5, 5, 5);
 }
 
 #pragma mark - getter and setter
@@ -46,6 +52,28 @@
     }
     
     return _textLabel;
+}
+
+- (UIView *)bgView {
+    if (!_bgView) {
+        _bgView = [UIView new];
+        _bgView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.1];
+        _bgView.layer.cornerRadius = 35.0 / 2;
+        _bgView.layer.masksToBounds = YES;
+    }
+    
+    return _bgView;
+}
+
+- (UIView *)eventView {
+    if (!_eventView) {
+        _eventView = [UIView new];
+        _eventView.backgroundColor = [UIColor colorWithRed:1 green:78.0 / 255.0 blue:0 alpha:1];
+        _eventView.layer.cornerRadius = 5.0 / 2;
+        _eventView.layer.masksToBounds = YES;
+    }
+    
+    return _eventView;
 }
 
 @end
